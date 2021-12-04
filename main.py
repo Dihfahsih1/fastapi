c2977c5440ef17f3307cda58581137f49e9ebd27
Dihfahsih1 <dihfahsihm@gmail.com>
2021-12-04 21:51:52 +0300

    create posts

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-  return{'message' : 'helloworld'}
+def get_posts():
+  return {'data' : 'These are your posts'}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+  return {'message': 'posts created successfully'}
